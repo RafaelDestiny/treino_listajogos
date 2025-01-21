e353b19c05d9a0c981ffc41cfaaf59190db15909
rafaeldestiny1984@gmail.com <rafaeldestiny1984@gmail.com>
2025-01-21 11:21:09 -0300

    Projeto criado

---
+++ src/main/java/com/treinospringboot/listadejogos/ListadejogosApplication.java
@@ -0,0 +1,13 @@
+package com.treinospringboot.listadejogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListadejogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListadejogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinospringboot/listadejogos/ListadejogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinospringboot.listadejogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListadejogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
