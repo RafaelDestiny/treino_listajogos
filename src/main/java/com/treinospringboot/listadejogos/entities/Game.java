c4b06800b0ae78f5e131223e16cdb718b28150b5
rafaeldestiny1984@gmail.com <rafaeldestiny1984@gmail.com>
2025-01-21 15:58:42 -0300

    properties, game, orm, gameseed

--- src/main/java/com/treinospringboot/listadejogos/entities/Game.java
+++ src/main/java/com/treinospringboot/listadejogos/entities/Game.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_games")
+@Table(name = "tb_game")
 public class Game {
 	
 	@Id
@@ -20,23 +20,27 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game(){
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -75,11 +79,21 @@ public class Game {
 	}
 
 	public String getPlataform() {
-		return plataform;
+		return platforms;
 	}
 
 	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+		this.platforms = plataform;
+	}
+	
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
